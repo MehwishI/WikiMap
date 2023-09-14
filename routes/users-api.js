916b933e2cb32248ec7515644d14d49d939d8e2a
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-09-14 00:32:03 -0400

    started login route

--- routes/users-api.js
+++ routes/users-api.js
@@ -6,8 +6,16 @@
  */
 
 const express = require('express');
+const app = express()
 const router  = express.Router();
 const userQueries = require('../db/queries/users');
+
+const cookieSession = require('cookie-session')
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key']
+}));
 
 router.get('/', (req, res) => {
   userQueries.getUsers()
@@ -20,5 +28,24 @@ router.get('/', (req, res) => {
         .json({ error: err.message });
     });
 });
+
+router.get('/login/:id', (req, res) => {
+  //I think we might need a post login route too>
+  ///also unclear if we need to call a query at this point
+  //like to pull the users maps or the users favourites
+
+  //tiny app uses a post request first
+
+  if(req.session.userID) {
+    return res.redirect("/")
+  }
+})
+
+
+router.post('/login/:id'){
+  //const id = req.body.id
+  //call query to query id from database?
+  //if id present?
+  //redirect, and call query for maps?
 
 module.exports = router;
