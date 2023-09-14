56e571b47bade841baf89a584142f7595d2ce973
Rose Francis <rosennabs@gmail.com>
2023-09-14 17:15:06 -0600

    Refactor maps-api

--- routes/maps.js
+++ routes/maps.js
@@ -1,18 +1,9 @@
+
 const express = require('express');
 const router  = express.Router();
-const db = require('../db/connection');
 
 router.get('/', (req, res) => {
-  db.query(`SELECT * from maps
-  LEFT JOIN locations
-  on maps.id = locations.map_id
-  WHERE user_id = $1`, [req.cookies.user_id])
-  .then((data)=>{
-    res.json(data.rows)
-  })
-  //res.render('users');
+  res.render('maps');
 });
-
-
 
 module.exports = router;
