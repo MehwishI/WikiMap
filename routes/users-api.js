e9d4f7e45545ea8e7a85d0266a1683c804d08ce9
Mehwish Iqbal <mehwishned21@gmail.com>
2023-09-14 16:39:15 -0500

    Logout route

--- routes/users-api.js
+++ routes/users-api.js
@@ -13,11 +13,9 @@ const userQueries = require('../db/queries/users');
 // const cookieSession = require('cookie-session')
 const cookieParser = require('cookie-parser');
 
-<<<<<<< HEAD
-app.use(cookieParser());
-=======
+
 app.use(cookieParser())
->>>>>>> mehwish
+
 
 // app.use(cookieSession({
 //   name: 'session',
@@ -56,16 +54,13 @@ router.get('/login/:id', (req, res) => { // Comments from Rose: Again wondering 
   return res.redirect("/")
 });
 
-<<<<<<< HEAD
-=======
 
-    return res.redirect("/")
-
-})
 router.post('/logout',(req,res) => {
-  req.clearCookie();
+  console.log("hello logout route")
+    //.cookies["user_id"])
+  res.clearCookie('user_id');
+  return res.redirect("/")
 })
->>>>>>> mehwish
 
 
 module.exports = router;
