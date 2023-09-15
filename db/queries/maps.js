d6e896bff45f234d3a19181d4f68cef44cfb26fa
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-09-15 09:58:03 -0400

    trying to commit maps.js

--- db/queries/maps.js
+++ db/queries/maps.js
@@ -1,19 +1,6 @@
 const db = require('../connection');
 
-const getMaps = () => {
-  return db.query(`SELECT * from maps
-  LEFT JOIN locations
-  ON maps.id = locations.map_id;`)
-    .then(data => {
-      return data.rows;//jn returning an array of objects
-    });
-};
 
-//used by /api/pins/:mapid
-const getPinsByMapId = (id) => {
-  return db.query(`SELECT * FROM locations
-  WHERE id = locations.id`)
-}
 //
 
 module.exports = { getMaps };
