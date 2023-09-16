ccee8bdf9da21a0435e47af02a1191158ddf4807
Rose Francis <rosennabs@gmail.com>
2023-09-15 21:53:34 -0600

    Create some ejs files and a route for favourites

--- routes/favs-api.js
+++ routes/favs-api.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router  = express.Router();
 const favQueries = require('../db/queries/favourites');
 
-// CRUD api 
-// Create POST request
 
-// Read all GET
+// GET a list of all favourites of a user
 router.get('/', (req, res) => {
   favQueries.getFavourites()
   .then((favourites)=>{
@@ -14,11 +12,17 @@ router.get('/', (req, res) => {
 
 });
 
-// Read one GET
+// Allows authenticated users to favourite a map
+router.post('/favourites', (req, res) => {
+  // if (req.session.user_id) {
+  //   res.send ('User is logged in!')
+  // }
+  
+  //Extract user_id and map_id from the request
+  const { user_id, map_id } = req.body;
+})
 
-// Update POST
 
-// DELETE POST
 
 
 
