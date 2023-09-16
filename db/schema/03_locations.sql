f2c3c60371e31c71b4c1c2a782cd92920639153c
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-09-16 18:08:11 -0400

    amended locations schema to latitude-longitude frfrom longitude-latitude and verified all coordinates

--- db/schema/03_locations.sql
+++ db/schema/03_locations.sql
@@ -6,8 +6,8 @@ CREATE TABLE locations (
   title VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
-  longitude DOUBLE PRECISION NOT NULL,
   latitude DOUBLE PRECISION NOT NULL,
+  longitude DOUBLE PRECISION NOT NULL,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
 );
