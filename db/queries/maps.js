9c08c0ad020b1844f8d5dd7639371dbce1aa06d5
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-09-21 17:00:13 -0400

    add put request ajax call and update query function. Tested that map and location functions are stlil working. Committing before further edits

--- db/queries/maps.js
+++ db/queries/maps.js
@@ -41,28 +41,29 @@ const createMap = (paramsObj) => {
   return db.query(`INSERT INTO maps (user_id, uid, title, center_latitude, center_longitude, zoom_level)
   VALUES ($1, $2, $3, $4, $5, $6)
   RETURNING *`, data)
-  .then(data => {
+  .then((data) => {
     return data.rows[0]
   })
-  .catch((err)=> {
-    console.log("create map error message: ", err.message)
+  .catch((err) => {
+    console.log("create map error message: ", err)
   });
 
 };
 
-//jsut here for reference, delete later
-// const addUser = function (user) {
-//   return pool
-//   .query(`INSERT INTO users (name, email, password)
-//   VALUES ($1, $2, $3)
-//   RETURNING *`, [user.name, user.email, user.password])
-//   .then((result)=> {
-//     return result.rows[0]
-//   })
-//   .catch((err) => {
-//     console.log(err.message)
-//   })
-
-// };
+const editMap = (paramsObj => {
+  let data =[paramsObj.id, paramsObj.user_id, paramsObj.uid, paramsObj.title, paramsObj.center_latitude, paramsObj.center_longitude, paramsObj.zoom_level]
+  console.log("edit maps query function triggered")
+  //not updating map id, user_id, or uid
+  return db.query(`UPDATE title, center_latitude, center_longitude, zoom-level)
+  VALUES($1, $2, $3, $4)
+  WHERE paramsObj.id =${paramsObj.id}
+  RETURNING *`, data,)
+  .then((response)=>{
+    return response.rows[0]
+  })
+  .catch((err) => {
+    console.log("edit map error messsage: ", err)
+  })
+})
 
 module.exports = { getMaps, getPinsByMapId,getMapById, createMap };
